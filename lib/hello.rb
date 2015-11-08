0df91ed2a22dad294c040db5e33897be35c58fee
skipsuva <skip.suva@gmail.com>
2015-11-08 12:01:56 -0500

    learned about yeild

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello
 
+
+names = ["Tim", "Tom", "Jim"]
+def hello_t(names)
+  names.each do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+    end
+  end
 end
 
-# call your method here!
-
+hello_t(names)
