607277133b4509db25dd16818d3d25e665ed79c1
mikeappell <mikeappell@gmail.com>
2015-11-10 17:31:25 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello
-
+def hello_t(names)
+  if block_given?
+    names.each do |name|
+      yield(name)
+    end
+  end
 end
 
 # call your method here!
 
+# array = ["Tim", "Tom", "Jim"]
+# hello_t(array) {|name| puts "Sup #{name}!"}
