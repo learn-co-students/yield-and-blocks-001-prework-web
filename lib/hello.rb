65360372e77fe440fc9828324327f1691bb4d5f7
skipsuva <skip.suva@gmail.com>
2015-12-02 13:18:53 -0500

    go back and fix lab

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,8 @@
-
-
-names = ["Tim", "Tom", "Jim"]
-def hello_t(names)
-  names.each do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
+def hello_t(array)
+  idx = 0
+  while idx < array.size
+    yield(array[idx])
+    idx += 1
   end
+  array
 end
-
-hello_t(names)
