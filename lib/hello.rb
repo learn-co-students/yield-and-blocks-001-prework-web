3fc38d40246165f2ca0700eb015ee85de20955ff
klaubel <beltran.klaudia@hotmail.com>
2016-02-21 16:18:30 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
-end
-
-# call your method here!
-
+def hello_t(array)
+  if block_given?
+    for i in 0...array.length
+      if array[i].start_with?("T")
+        yield array[i]
+      end
+    end
+  else
+    puts "Please enter a block!"
+  end
+  array
+  end     
