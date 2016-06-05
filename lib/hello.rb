bbf0586b3ec9bb958eee239d81ea7371714bee47
debashish2549 <debashish.deep74@qmail.cuny.edu>
2016-06-05 01:13:55 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
-
+def hello_t (array)
+  if block_given?
+    for i in 0..array.length-1
+      yield (array[i])
+    end
+    array
+  else
+    puts "There is no block available!"
+  end
 end
 
-# call your method here!
+
+hello_t (["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")  
+    puts "Hi, #{name}"
+  end
+end
+
+
+
 
