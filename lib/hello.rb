212bda6281111f9be33c567e18a09049b0959d56
Michael Wiggins <michael.wiggins@me.com>
2016-08-20 17:26:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,9 +2,9 @@ def hello_t(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i = i + 1
+    i += 1
   end
   array
 end
-#This shit made no sense until I read this: https://www.codecademy.com/en/forum_questions/51c72e759c4e9d410501df42
-#yield adds flexibility to your method. 
+#Bookmark: https://www.codecademy.com/en/forum_questions/51c72e759c4e9d410501df42
+#yield adds flexibility to your method.
